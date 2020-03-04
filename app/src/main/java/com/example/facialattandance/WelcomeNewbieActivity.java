c7325914e35f225943dfd062c1f049fc8d5325a0
vanna.kimhai <v-kimhai@users.noreply.github.com>
2020-03-04 14:57:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/facialattandance/WelcomeNewbieActivity.java
@@ -0,0 +1,22 @@
+package com.example.facialattandance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class WelcomeNewbieActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_newbie);
+        Button go = (Button) findViewById(R.id.go);
+        
+    }
+}
