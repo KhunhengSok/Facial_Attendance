73b04e04793bd85043277d0f204b95c3290076c8
vanna.kimhai <v-kimhai@users.noreply.github.com>
2020-03-04 16:31:16 +0700

    profile and login

--- app/src/main/java/com/example/facialattandance/ProfileActivity.java
+++ app/src/main/java/com/example/facialattandance/ProfileActivity.java
@@ -2,15 +2,13 @@ package com.example.facialattandance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_profile);
     }
 }
