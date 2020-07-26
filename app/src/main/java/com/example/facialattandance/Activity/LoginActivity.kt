1085bcac29a4edbe189a7d56802f60f9f2ccb7a8
KhunhengSok <34951730+KhunhengSok@users.noreply.github.com>
2020-07-26 16:49:34 +0700

    Add loading progress bar in LoginActivity.kt

--- app/src/main/java/com/example/facialattandance/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/facialattandance/Activity/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.AuthFailureError
@@ -24,54 +26,66 @@ class LoginActivity : AppCompatActivity() {
     companion object{
         val TAG = "LoginActivity"
     }
+
     private val LOGIN_URL = "https://face-attendance-api.herokuapp.com/api/account/login"
     val PERMISSION_FINISH = false
     var requestQueue:RequestQueue ?= null
+    private var isLoading  = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        showLoading(false)
         init()
-
     }
 
     private fun init() {
         requestQueue = Volley.newRequestQueue(this)
         loginButton.setOnClickListener(View.OnClickListener {
+            Log.d(TAG, "init: Log in button clicked")
             val intent = Intent(applicationContext, HomeActivity::class.java)
 
-//            val request: RequestQueue = Volley.newRequestQueue(this)
             var username = usernameEdit.text.toString()
             var password = passwordEdit.text.toString()
 
-            val requestBody = JSONObject()
-            requestBody.put("username", username)
-            requestBody.put("password", password)
+            if( !username.isBlank() && !password.isBlank() && !isLoading){
+                showLoading(true)
 
-            val request = JsonObjectRequest(Request.Method.POST, LOGIN_URL, requestBody, Response.Listener {
-                response ->
-                run {
-                    try {
-                        Log.d(TAG, "init: response")
-                        val jsonObject = response
-                        val token = jsonObject.getString("token")
-                        val username = jsonObject.getString("username")
-                        //                            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
-                        if (username == usernameEdit.text.toString()) {
-                            finish()
-                            startActivity(intent)
+                val requestBody = JSONObject()
+                requestBody.put("username", username)
+                requestBody.put("password", password)
+
+                val request = JsonObjectRequest(Request.Method.POST, LOGIN_URL, requestBody, Response.Listener {
+                    response ->
+                        run {
+                            try {
+                                Log.d(TAG, "init: response")
+                                val jsonObject = response
+                                val token = jsonObject.getString("token")
+                                val username = jsonObject.getString("username")
+                                //                            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
+                                if (username == usernameEdit.text.toString()) {
+                                    finish()
+                                    startActivity(intent)
+                                }
+                            } catch (e: JSONException) {
+                                e.printStackTrace()
+                                Toast.makeText(this, "Login Error!$e", Toast.LENGTH_SHORT).show()
+                            }
+                            showLoading(false)
+
                         }
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                        Toast.makeText(this, "Login Error!$e", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }, Response.ErrorListener {error ->
-                Toast.makeText(this, "Login Error!$error", Toast.LENGTH_SHORT).show()
-            } )
-            requestQueue!!.add(request)
+                }, Response.ErrorListener {error ->
+                    Toast.makeText(this, "Login Error!$error", Toast.LENGTH_SHORT).show()
+                    showLoading(false)
 
+                } )
+                requestQueue!!.add(request)
+            }else if (username.isBlank() || password.isBlank()){
+                Log.d(TAG, "init: Blank ")
+                Toast.makeText(baseContext, "Please fill the username and password properly.", Toast.LENGTH_SHORT).show()
+            }
 
             /*val stringRequest: StringRequest = object : StringRequest(Method.POST, LOGIN_URL,
                     Response.Listener { response ->
@@ -100,9 +114,27 @@ class LoginActivity : AppCompatActivity() {
                     param.put("password", password)
                     return param
                 }
-            }*/
+            }
 
-//            requestQueue!!.add(stringRequest)
+            requestQueue!!.add(stringRequest)
+*/
         })
+    }
+
+    fun showLoading(loading:Boolean){
+        isLoading = loading
+        Log.d(TAG, "showLoading: ${loading}")
+        if(isLoading){
+            loginProgressBar.visibility = ProgressBar.VISIBLE
+            blurBackground.visibility = LinearLayout.VISIBLE
+            usernameEdit?.isEnabled = false
+            passwordEdit?.isEnabled = false
+        }else{
+            loginProgressBar.visibility = ProgressBar.INVISIBLE
+            blurBackground.visibility = LinearLayout.INVISIBLE
+            usernameEdit?.isEnabled = true
+            passwordEdit?.isEnabled = true
+        }
+
     }
 }
