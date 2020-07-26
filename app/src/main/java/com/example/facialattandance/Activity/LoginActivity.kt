a8d778506f6139ca5824c9dbefd7c91185469359
KhunhengSok <34951730+KhunhengSok@users.noreply.github.com>
2020-07-26 18:21:05 +0700

    Add always remember user functionality and temporary logout button

--- app/src/main/java/com/example/facialattandance/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/facialattandance/Activity/LoginActivity.kt
@@ -13,6 +13,7 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.facialattandance.Activity.SplashScreenActivity.Companion.saveUser
 import com.example.facialattandance.R
 import com.example.facialattandance.utils.HOST_URL
 import kotlinx.android.synthetic.main.activity_login.*
@@ -59,6 +60,9 @@ class LoginActivity : AppCompatActivity() {
                         run {
                             try {
                                 Log.d(TAG, "init: response")
+
+                                saveUser(response)
+
                                 val jsonObject = response
                                 val token = jsonObject.getString("token")
                                 val username = jsonObject.getString("username")
@@ -85,39 +89,9 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(baseContext, "Please fill the username and password properly.", Toast.LENGTH_SHORT).show()
             }
 
-            /*val stringRequest: StringRequest = object : StringRequest(Method.POST, LOGIN_URL,
-                    Response.Listener { response ->
-                        try {
-                            val jsonObject = JSONObject(response)
-                            val token = jsonObject.getString("token")
-                            val username = jsonObject.getString("username")
-//                            Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
-                            if (username == usernameEdit.text.toString()) {
-                                finish()
-                                startActivity(intent)
-                            }
-                        } catch (e: JSONException) {
-                            e.printStackTrace()
-                            Toast.makeText(this, "Login Error!$e", Toast.LENGTH_SHORT).show()
-                        }
-                    },
-                    Response.ErrorListener { error ->
-                        Toast.makeText(this, "Login Error!$error", Toast.LENGTH_SHORT).show()
-                    }) {
-
-
-                override fun getParams(): MutableMap<String, String> {
-                    var param = HashMap<String,String>()
-                    param.put("username", username)
-                    param.put("password", password)
-                    return param
-                }
-            }
-
-            requestQueue!!.add(stringRequest)
-*/
         })
     }
+
 
     fun showLoading(loading:Boolean){
         isLoading = loading
@@ -135,4 +109,6 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/facialattandance/utils/UserAccountUtil.kt
@@ -0,0 +1,5 @@
+package com.example.facialattandance.utils
+
+class UserAccountUtil {
+
+}
